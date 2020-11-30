324db210cede97bb3a9cf30784d8ed7a67aaac50
DESKTOP-OV2UEPO\Andrej <dimec.andrej@gmail.com>
2020-11-30 21:29:20 +0100

    dva

---
+++ Gradbeni dnevnik/frmSodelavci.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+//using System.Drawing;
+
+namespace Komunala
+{
+    public partial class frmSodelavci : Form
+    {
+        SqlConnection con = frmMain.c;
+        SqlConnection con2 = frmMain.c2;
+        SqlConnection con3 = frmMain.c3;
+
+        SqlCommand cmd;
+        SqlDataReader rdr = null;
+
+        int dodaj;
+        string tskupina = "";
+        string index;
+        string stindeks;
+        int tid, tindeks;
+
+        private Color ColorOzadje;
+        //public Color ColorOzadje
+        //{
+        //    get { return ColorOozadje; }
+        //    set { ColorOOzadje = value; Invalidate(); }
+        //}
+        ////private Color ColorOzadje;
+        //Color ColorOzadje = new Color.FromArgb(0, 62, 107);  // ozadje neaktivnega
+
+        public frmSodelavci()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSodelavci_Load(object sender, EventArgs e)
+        {
+            onemogoci_tb();
+            Grid();
+            Display();
+            Zacetek();
+        }
+
+        private void Design()
+        {
+           //  frmSodelavci.DefaultBackColor = Color_ozadje;
+        }
+        private void Izprazni_dgv()
+        {
+            dgv1.Rows.Clear();
+            dgv1.Refresh();
+        }
+
+        private void onemogoci_tb()
+        {
+            tb1.Enabled = false;
+            tb2.Enabled = false;
+        }
+
+        private void omogoci_tb()
+        {
+            tb1.Enabled = true;
+            tb2.Enabled = true;
+        }
+
+        private void izprazni_tb()
+        {
+            tb1.Text = "";
+            tb2.Text = "";
+        }
+
+        private void Grid()
+        {
+            dgv1.ColumnHeadersVisible = false;
+            dgv1.RowHeadersVisible = false;
+            dgv1.ColumnCount = 3;
+            dgv1.Columns[1].Width = 240;
+            dgv1.Columns[2].Width = 60;
+            dgv1.Columns[0].Name = "Id";
+            dgv1.Columns[1].Name = "Skupina";
+            dgv1.Columns[2].Name = "Indeks";
+            dgv1.Columns["Id"].Visible = false;
+            this.dgv1.Columns[1].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            this.dgv1.Columns[2].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgv1.Focus();
+
+        }
+
+        private void Display()
+        {
+            Izprazni_dgv();
+            string q = "select * from tblSkupinestoritev order by indeks";
+            try
+            {
+                cmd = new SqlCommand(q, con);
+                con.Open();
+                rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    tid = (int)rdr["Id"];
+                    tskupina = (string)rdr["skupina"]; // Opis
+                    tindeks = (int)rdr["indeks"];  // določi id v katerega boš pisal
+                    stindeks = Convert.ToString(tindeks);
+
+                    string strid = Convert.ToString(tid);
+                    string[] row1 = new string[] { strid, tskupina, stindeks };
+                    dgv1.Rows.Add(row1);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Napaka: " + ex.Message);
+            }
+            finally
+            {
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+                if (con != null)
+                {
+                    con.Close();
+                }
+            }
+            //Zacetek();
+        }
+
+        private void Zacetek()
+        {
+            dodaj = 0;
+            izprazni_tb();
+            onemogoci_tb();
+            btnShrani.Enabled = false;
+            btnPreklici.Enabled = false;
+            btnDodaj.Enabled = true;
+            btnBrisi.Enabled = true;
+            btnSpremeni.Enabled = true;
+            btnNazaj.Enabled = true;
+            dgv1.Focus();
+        }
+
+        private void Brisi()
+        {
+            DialogResult result = MessageBox.Show("Izbrišem zapis?", "Potrdi brisanje", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    string q = "delete from tblSkupinestoritev where id = @tid";
+                    cmd = new SqlCommand(q, con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@tid", tid);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Napaka pri brisanju: " + ex.Message);
+                }
+                finally
+                {
+                    if (con != null)
+                    {
+                        con.Close();
+                    }
+                }
+                Zacetek();
+            }
+            else if (result == DialogResult.No)
+            {
+                Zacetek();
+            }
+            Display();
+        }
+
+        private void Shrani()
+        {
+            string q;
+            if (tb1.Text != "" && tb2.Text != "")
+            {
+                tskupina = tb2.Text;
+                stindeks = tb1.Text;
+                tindeks = Convert.ToInt32(stindeks);
+                try
+                {
+                    if (dodaj == 1)
+                    {
+                        // dodaj
+                        q = "insert into tblSkupinestoritev (skupina,indeks) values(@tskupina,@tindeks)";
+                        cmd = new SqlCommand(q, con);
+                        con.Open();
+                        cmd.Parameters.AddWithValue("@tskupina", tskupina);
+                        cmd.Parameters.AddWithValue("@tindeks", tindeks);
+                        cmd.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        // spremeni
+                        q = "update tblSkupinestoritev set skupina = @tskupina, indeks= @tindeks where id=@tid";
+                        cmd = new SqlCommand(q, con);
+                        con.Open();
+                        cmd.Parameters.AddWithValue("@tid", tid);
+                        cmd.Parameters.AddWithValue("@tskupina", tskupina);
+                        cmd.Parameters.AddWithValue("@tindeks", tindeks);
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Napaka: " + ex.Message);
+                }
+                finally
+                {
+                    if (con != null)
+                    {
+                        con.Close();
+                    }
+                }
+                Zacetek();
+
+            }
+            else
+            {
+                MessageBox.Show("Napaka: Polje ne sme biti prazno.");
+                tb1.Focus();
+            }
+            Display();
+        }
+
+        private void Dodaj()
+        {
+            dodaj = 1;
+            omogoci_tb();
+            izprazni_tb();
+            btnShrani.Enabled = true;
+            btnPreklici.Enabled = true;
+            btnDodaj.Enabled = false;
+            btnBrisi.Enabled = false;
+            btnSpremeni.Enabled = false;
+            btnNazaj.Enabled = false;
+            tb1.Focus();
+        }
+
+        private void Spremeni()
+        {
+            dodaj = 0;
+            omogoci_tb();
+            btnShrani.Enabled = true;
+            btnPreklici.Enabled = true;
+            btnDodaj.Enabled = false;
+            btnBrisi.Enabled = false;
+            btnSpremeni.Enabled = false;
+            btnNazaj.Enabled = false;
+            tb1.Focus();
+        }
+        private void Preklici()
+        {
+            izprazni_tb();
+            Zacetek();
+        }
+    }
+}
---
+++ Gradbeni dnevnik/frmSodelavci.designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Komunala
+{
+    partial class frmSodelavci
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        //private System.Windows.Forms.Button btnSpremeni;
+
+        private void InitializeComponent()
+        {
+            this.btnSpremeni = new System.Windows.Forms.Button();
+            this.btnBrisi = new System.Windows.Forms.Button();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnNazaj = new System.Windows.Forms.Button();
+            this.btnPreklici = new System.Windows.Forms.Button();
+            this.btnShrani = new System.Windows.Forms.Button();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSpremeni
+            // 
+            this.btnSpremeni.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(62)))), ((int)(((byte)(107)))));
+            this.btnSpremeni.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSpremeni.ForeColor = System.Drawing.Color.White;
+            this.btnSpremeni.Location = new System.Drawing.Point(198, 557);
+            this.btnSpremeni.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSpremeni.Name = "btnSpremeni";
+            this.btnSpremeni.Size = new System.Drawing.Size(136, 44);
+            this.btnSpremeni.TabIndex = 26;
+            this.btnSpremeni.Text = "Spremeni";
+            this.btnSpremeni.UseVisualStyleBackColor = false;
+            // 
+            // btnBrisi
+            // 
+            this.btnBrisi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(62)))), ((int)(((byte)(107)))));
+            this.btnBrisi.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnBrisi.ForeColor = System.Drawing.Color.White;
+            this.btnBrisi.Location = new System.Drawing.Point(355, 557);
+            this.btnBrisi.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnBrisi.Name = "btnBrisi";
+            this.btnBrisi.Size = new System.Drawing.Size(136, 44);
+            this.btnBrisi.TabIndex = 30;
+            this.btnBrisi.Text = "Briši";
+            this.btnBrisi.UseVisualStyleBackColor = false;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(62)))), ((int)(((byte)(107)))));
+            this.btnDodaj.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDodaj.ForeColor = System.Drawing.Color.White;
+            this.btnDodaj.Location = new System.Drawing.Point(40, 557);
+            this.btnDodaj.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(136, 44);
+            this.btnDodaj.TabIndex = 31;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = false;
+            // 
+            // btnNazaj
+            // 
+            this.btnNazaj.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(62)))), ((int)(((byte)(107)))));
+            this.btnNazaj.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnNazaj.ForeColor = System.Drawing.Color.White;
+            this.btnNazaj.Location = new System.Drawing.Point(355, 618);
+            this.btnNazaj.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnNazaj.Name = "btnNazaj";
+            this.btnNazaj.Size = new System.Drawing.Size(136, 44);
+            this.btnNazaj.TabIndex = 27;
+            this.btnNazaj.Text = "Izhod";
+            this.btnNazaj.UseVisualStyleBackColor = false;
+            // 
+            // btnPreklici
+            // 
+            this.btnPreklici.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(62)))), ((int)(((byte)(107)))));
+            this.btnPreklici.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPreklici.ForeColor = System.Drawing.Color.White;
+            this.btnPreklici.Location = new System.Drawing.Point(198, 618);
+            this.btnPreklici.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnPreklici.Name = "btnPreklici";
+            this.btnPreklici.Size = new System.Drawing.Size(136, 44);
+            this.btnPreklici.TabIndex = 28;
+            this.btnPreklici.Text = "Prekliči";
+            this.btnPreklici.UseVisualStyleBackColor = false;
+            // 
+            // btnShrani
+            // 
+            this.btnShrani.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(62)))), ((int)(((byte)(107)))));
+            this.btnShrani.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnShrani.ForeColor = System.Drawing.Color.White;
+            this.btnShrani.Location = new System.Drawing.Point(40, 618);
+            this.btnShrani.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnShrani.Name = "btnShrani";
+            this.btnShrani.Size = new System.Drawing.Size(136, 44);
+            this.btnShrani.TabIndex = 29;
+            this.btnShrani.Text = "Shrani";
+            this.btnShrani.UseVisualStyleBackColor = false;
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(12, 12);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.Size = new System.Drawing.Size(350, 252);
+            this.dgv1.TabIndex = 32;
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(513, 100);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(234, 26);
+            this.tb1.TabIndex = 33;
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(513, 154);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(234, 26);
+            this.tb2.TabIndex = 34;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(32)))), ((int)(((byte)(77)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(40, 468);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 42);
+            this.button1.TabIndex = 35;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // frmSodelavci
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1212, 694);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.btnBrisi);
+            this.Controls.Add(this.btnShrani);
+            this.Controls.Add(this.btnPreklici);
+            this.Controls.Add(this.btnNazaj);
+            this.Controls.Add(this.btnSpremeni);
+            this.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmSodelavci";
+            this.Text = "frmSodelavci";
+            this.Load += new System.EventHandler(this.frmSodelavci_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSpremeni;
+        private System.Windows.Forms.Button btnBrisi;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnNazaj;
+        private System.Windows.Forms.Button btnPreklici;
+        private System.Windows.Forms.Button btnShrani;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Button button1;
+    }
+}
